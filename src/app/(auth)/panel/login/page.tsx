10028ef3f9a84120c24a4b0df64ad2996678bff0
Gg777ok <agusutir22@gmail.com>
2026-08-24 00:38:46 +0700

    init

--- src/app/(auth)/panel/login/page.tsx
+++ src/app/(auth)/panel/login/page.tsx
@@ -1,16 +1,18 @@
-import { LoginForm } from "@/app/(auth)/panel/login/components/form";
 import { createClient } from "@/lib/server";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
+import { LoginForm } from "./components/form";
 
 export const metadata: Metadata = {
   title: "LIVEMOTOGP",
-}; 
+};
 
 export default async function LoginPage() {
-  const supabase = await createClient()
-  const { data: {user}} = await supabase.auth.getUser()
-  if(user) redirect("/panel/dashboard")
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user) redirect("/panel/dashboard");
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-2 md:p-5">
       <div className="w-full max-w-sm md:max-w-2xl">
